b774464a1646c6d1e50bfa19ea5d9e6b9bbec0a7
Marina Avramenko <marina.avramenko123@gmail.com>
2024-12-17 19:42:35 +0200

    clone phone_catalog

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,17 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+import { store } from './app/store';
 import { App } from './App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
+  </StrictMode>,
+);
